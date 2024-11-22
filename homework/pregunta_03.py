578c5a061cc5b05f98c10b03fcded836d5a5eb1e
David Leonardo Pabon Nuñez <94508095+DavLeonardo@users.noreply.github.com>
2024-11-22 00:05:16 -0500

    Solucion

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,33 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    ruta = "files/input/data.csv"
+    letra = []
+    numero = []
+    ListaLetras = []
+    SumaNumeros = []
+
+    with open(ruta, "r", encoding="utf-8") as files:
+        lines = files.readlines()
+
+    data = [linea.strip().split("\t") for linea in lines]
+
+    for i in range(len(data)):
+        letra.append(data[i][0])
+        numero.append(data[i][1])
+
+    #! lista de las letras
+    for i in range(len(letra)):
+        if not (letra[i] in ListaLetras):
+            ListaLetras.append(letra[i])
+            SumaNumeros.append(0)
+    ListaLetras.sort()
+
+    #! suma de los numeros
+    for i in range(len(ListaLetras)):
+        for j in range(len(letra)):
+            if letra[j] == ListaLetras[i]:
+                SumaNumeros[i] += int(numero[j])
+
+    out = list(zip(ListaLetras, SumaNumeros))
+    return out
