6058925bed52ee93f8cfb52947719eaf9985bd46
DavLeonardo <dlpabonn@unal.edu.co>
2024-12-08 13:01:10 -0500

    Complete

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -25,3 +25,35 @@ def pregunta_07():
      (9, ['A', 'B', 'E', 'A', 'A', 'C'])]
 
     """
+    ruta = "files/input/data.csv"
+    numeros = []
+    letras = []
+    numerosUnicos = []
+    with open(ruta, "r", encoding="utf-8") as files:
+        lines = files.readlines()
+    data = [linea.strip().split("\t") for linea in lines]
+
+    for i in range(len(data)):
+        numeros.append(data[i][1])
+
+    for i in range(len(numeros)):
+        if not (numeros[i] in numerosUnicos):
+            numerosUnicos.append(numeros[i])
+    numerosUnicos.sort()
+
+    for i in range(len(numerosUnicos)):
+        x = []
+        for j in range(len(data)):
+            if numerosUnicos[i] == data[j][1]:
+                x.append(data[j][0])
+        letras.append(x)
+
+    numerosUnicos = list(map(int, numerosUnicos))
+
+    out = list(
+        zip(
+            numerosUnicos,
+            letras,
+        )
+    )
+    return out
